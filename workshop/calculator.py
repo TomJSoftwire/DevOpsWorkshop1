87aef25e189c460b7aa200ac03993681b6c595da
Tom Johnston <thomas.johnston@softwire.com>
2021-10-12 13:54:48 +0100

    added mod operator

--- workshop/calculator.py
+++ workshop/calculator.py
@@ -1,6 +1,6 @@
 import math
 
-allowed_operators = {'/','x','+','-'}
+allowed_operators = {'/','x','+','-','%'}
 
 def parse_calculation (parameter_list):
 
@@ -31,6 +31,8 @@ def execute_calculation(parsed_calculation):
             return first_num * second_num
         case '/':
             return first_num / second_num
+        case '%':
+            return first_num % second_num
 
 def handle_calculation(calc):
     return execute_calculation(parse_calculation(calc))
